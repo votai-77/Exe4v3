418e44401546a71282e64ce4b54f6c97c2c566a0
votai-77 <vothanhtai823@gmail.com>
2020-08-01 09:11:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exe4/CustomMusicAdapter.java
@@ -0,0 +1,5 @@
+package com.example.exe4;
+
+public class CustomMusicAdapter {
+
+}
---
+++ app/src/main/java/com/example/exe4/Music.java
@@ -0,0 +1,31 @@
+package com.example.exe4;
+
+public class Music {
+    String name;
+    String singer;
+    int song;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public int getSong() {
+        return song;
+    }
+
+    public void setSong(int song) {
+        this.song = song;
+    }
+}
